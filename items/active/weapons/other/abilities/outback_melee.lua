4e61bca2245eb853970b25139a6eb020d2d68c09
Nebulox <42601049+Nebulox@users.noreply.github.com>
2021-12-24 15:39:04 +1000

    Weedwhacker Update
    - Cleaned up file organization
    - Removed file bloat
    - Adjusted some values
    - Updated the combo to allow for weedwhacking and tiledamaging

--- items/active/weapons/other/abilities/outback_melee.lua
+++ items/active/weapons/other/abilities/outback_melee.lua
@@ -25,37 +25,6 @@ function MeleeCombo:init()
   end
 end
 
-local oldSetStance = self.weapon.setStance
-function self.weapon:setStance(stance)
-	oldSetStance(self, stance)
-	
-	--moved your dumb flip thing here and made it less shit
-	animator.setFlipped(stance.flipx == true)
-	
-	
-	local air = not mcontroller.groundMovement()
-	local velocity = air and stance.airVelocity or stance.velocity
-	
-	if velocity then
-		local v = copy(velocity)
-		
-		v[1] = v[1] * mcontroller.facingDirection()
-		if not stance.resetVelocity then
-			v = vec2.add(v, mcontroller.velocity())
-		end
-		
-		mcontroller.setVelocity(v)
-	end
-end
-
-function MeleeCombo:unsheathe()
-	if not self.stances.unsheathe then return end
-	
-  self.weapon:setStance(self.stances.unsheathe)
-  util.wait(self.stances.unsheathe.duration)
-  return true
-end
-
 -- Ticks on every update regardless if this is the active ability
 function MeleeCombo:update(dt, fireMode, shiftHeld)
   WeaponAbility.update(self, dt, fireMode, shiftHeld)
@@ -160,8 +129,12 @@ function MeleeCombo:fire()
   animator.setParticleEmitterOffsetRegion(swooshKey, self.swooshOffsetRegions[self.comboStep])
   animator.burstParticleEmitter(swooshKey)
 
+  local damageArea = partDamageArea("swoosh")
+  if self.canCut then
+    self:damageTiles(damageArea)
+  end
+  
   util.wait(stance.duration, function()
-    local damageArea = partDamageArea("swoosh")
     self.weapon:setDamage(self.stepDamageConfig[self.comboStep], damageArea)
   end)
 
@@ -171,6 +144,34 @@ function MeleeCombo:fire()
   else
     self.cooldownTimer = self.cooldowns[self.comboStep]
     self.comboStep = 1
+  end
+end
+
+--Hi is me the nebby boi - Nebulox if you're dumb
+function MeleeCombo:damageTiles(area)
+  local miningPositions = self:nebFindAllPointsInPolygon(area)
+  --Calculate tile damage per frame
+  local levelMultiplier = 1
+  if self.considerLevel then 
+    levelMultiplier = config.getParameter("level", 1)
+  end
+  local tileDamage = levelMultiplier * self.tileDamagePerSwing
+  
+  --Reset damageTiles at the start of every frame
+  self.damagingTiles = false
+  
+  --Optionally damage foreground tiles
+  if self.damageForeground and tileDamage > 0 and self.tileDamagePerSwing > 0 then
+	if world.damageTiles(miningPositions, "foreground", mcontroller.position(), self.tileDamageType, tileDamage, 99) then
+	  self.damagingTiles = true
+	end
+  end
+  
+  --Optionally damage background tiles
+  if self.damageBackground and tileDamage > 0 and self.tileDamagePerSwing > 0 then
+	if world.damageTiles(miningPositions, "background", mcontroller.position(), self.tileDamageType, tileDamage, 99) then
+	  self.damagingTiles = true
+	end
   end
 end
 
@@ -216,6 +217,60 @@ function MeleeCombo:computeDamageAndCooldowns()
     local speedFactor = 1.0 * (self.comboSpeedFactor ^ i)
     table.insert(self.cooldowns, (targetTime - totalAttackTime) * speedFactor)
   end
+end
+
+--Helper functions and hooks
+local oldSetStance = self.weapon.setStance
+function self.weapon:setStance(stance)
+  oldSetStance(self, stance)
+  
+  animator.setFlipped(stance.flipx == true)
+
+  local velocity = (not mcontroller.groundMovement()) and stance.airVelocity or stance.velocity
+  if velocity then
+	local v = copy(velocity)
+		
+	v[1] = v[1] * mcontroller.facingDirection()
+	if not stance.resetVelocity then
+	  v = vec2.add(v, mcontroller.velocity())
+	end	
+	mcontroller.setVelocity(v)
+  end
+end
+
+function MeleeCombo:unsheathe()
+  if not self.stances.unsheathe then return end
+
+  self.weapon:setStance(self.stances.unsheathe)
+  util.wait(self.stances.unsheathe.duration)
+  return true
+end
+
+function MeleeCombo:nebFindAllPointsInPolygon(poly)
+  local first = poly[1]
+  if not first then return {} end
+  local contains = world.polyContains
+  local minX, maxX = first[1], first[1]
+  local minY, maxY = first[2], first[2]
+  for i = 1, #poly do
+    local point = poly[i]
+    local x, y = point[1], point[2]
+    if     minX > x then minX = x
+    elseif maxX < x then maxX = x end
+    if     minY > y then minY = y
+    elseif maxY < y then maxY = y end
+  end
+  local t = {}
+  local tI = 0
+  for x = math.ceil(minX - 0.5), math.floor(maxX - 0.5) do
+      for y = math.ceil(minY - 0.5), math.floor(maxY - 0.5) do
+        if contains(poly, {x + 0.5, y + 0.5}) then
+          tI = tI + 1
+          t[tI] = vec2.add(mcontroller.position(), {x, y})
+        end
+      end
+  end
+  return t
 end
 
 function MeleeCombo:uninit()
